85ccee6d1438f586abf2e20a669dba69f60f320a
tew9 <tango.tew@outlook.com>
2020-04-16 17:17:57 -0500

    add documentations for models [172282272]

--- angular/src/app/data/booking/guest.model.ts
+++ angular/src/app/data/booking/guest.model.ts
@@ -1,5 +1,13 @@
+
+/**
+ * Represents the number of guesses reserving the rooms.
+ * @constructor
+ */
+
 export class Guest {
 	guestId: number;
 	guestType: string;
+  guessFirstName: string;
+  guessLastName: string;
 	constructor() {}
 }
--- angular/src/app/data/booking/reservation.model.ts
+++ angular/src/app/data/booking/reservation.model.ts
@@ -2,6 +2,11 @@ import { Guest } from './guest.model';
 import { Duration } from './duration.model';
 import { Status } from './status.model';
 
+/**
+ * Represents a book.
+ * @constructor
+ */
+
 export class Reservation {
   reservationId: number;
   accountId: number;
