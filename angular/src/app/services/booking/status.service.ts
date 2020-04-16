2cb494dd8f537cf5d2271f66ea52c5a5d9c9d71c
tew9 <tango.tew@outlook.com>
2020-04-16 17:56:47 -0500

    Change httpService with httpClient

--- angular/src/app/services/booking/status.service.ts
+++ angular/src/app/services/booking/status.service.ts
@@ -1,6 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpHeaders } from '@angular/common/http';
-import { HttpService } from '../http/http.service';
+import { HttpHeaders, HttpClient } from '@angular/common/http';
 import { Observable, of } from 'rxjs';
 import { Status } from '../../data/booking/status.model';
 
@@ -12,30 +11,32 @@ export class StatusService {
   httpOptions = {
     headers: new HttpHeaders({ 'Content-Type': 'application/json'})
   }
-  
-  constructor(private _http: HttpService) { }
+
+  constructor(private _http: HttpClient) { }
 
   get(): Observable<Status[]> {
-    return this._http.get_async(this.url)
+    return this._http.get(this.url)
                       .pipe(this.handleError("Error in get Guest"));
   }
-  
+
   post(status: Status): Observable<Status> {
 
-    return this._http.post_async(this.url, status) 
+    return this._http.post(this.url, status)
                       .pipe(this.handleError<Status>("post error")) as Observable<Status>
-                      
+
   }
 
   put(status: Status): Observable<Status> {
-    return this._http.put_async(this.url, status)
+    return this._http.put(this.url, status)
                       .pipe(this.handleError<Status>("Error in put Status")) as Observable<Status>;
   }
 
-  delete() {
-    
+  delete(statusId: number): Observable<Status> {
+    const url = `${this.url}/${statusId}`;
+    return this._http.delete(url)
+                .pipe(this.handleError<Status>("Error in delete Status"));
   }
-  
+
   private handleError<T> (operation = 'operation', result?: T) {
     return (error: any): Observable<T> => {
       return of(result as T)
