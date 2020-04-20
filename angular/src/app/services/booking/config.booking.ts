0c8695f3d2e567841329d7f45f2c16795475af26
Roby Thomas <roby.thomas1020@gmail.com>
2020-04-20 16:44:06 -0500

    fix config lint [#172282272]

--- angular/src/app/services/booking/config.booking.ts
+++ angular/src/app/services/booking/config.booking.ts
@@ -1,6 +1,6 @@
 export class Config {
-  reservation = { uri: "api/reservation" }
-  status = { uri: "api/status" }
-  duration = { uri: "api/duration" }
-  guest = { uri: "api/guest" }
+  reservation = { uri: 'api/reservation' };
+  status = { uri: 'api/status' };
+  duration = { uri: 'api/duration' };
+  guest = { uri: 'api/guest' };
 }
