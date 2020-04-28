4d4d2ad7c20b78ca48f27c027509614489f140cd
tew9 <tango.tew@outlook.com>
2020-04-27 22:06:18 -0500

    date added

--- angular/src/app/modules/booking/booking.module.ts
+++ angular/src/app/modules/booking/booking.module.ts
@@ -4,6 +4,8 @@ import { CommonModule } from '@angular/common';
 import { BookingRoutingModule } from './booking-routing.module';
 import { BookingSearchComponent } from './booking/booking-search/booking-search.component';
 import { BookingFilterComponent } from './booking/booking-filter/booking-filter.component';
+import { Stay } from '../../data/stay.model';
+import { NgForm } from '@angular/forms';
 
 @NgModule({
   declarations: [BookingSearchComponent],
--- angular/src/app/modules/booking/booking/booking-search/booking-search.component.ts
+++ angular/src/app/modules/booking/booking/booking-search/booking-search.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Subject, Observable } from 'rxjs';
+import { debounceTime, distinctUntilChanged, switchMap } from 'rxjs/operators';
 
 @Component({
   selector: 'uic-booking-search',
@@ -7,9 +9,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BookingSearchComponent implements OnInit {
 
+  @Input() cityName: string;
+  // cities: string[];
+  cities :string[] = ["Dallas", "Arlington", "Fortworth"];
+  private searchTerms = new Subject<String>();
+  //cities$: Observable<String[]>;
+
   constructor() { }
 
   ngOnInit(): void {
+  // // wait 300ms after each keystroke before considering the term
+  //   debounceTime(300),
+
+    // // ignore new term if same as previous term
+    // distinctUntilChanged(),
+
+    // // switch to new search observable each time the term changes
+    // switchMap((term: string) => this.val)
+
+    // switchMap((term: string) => this.heroService.searchHeroes(term)),
+  }
+
+  search(term: string): void {
+    this.searchTerms.next(term);
   }
 
 }
