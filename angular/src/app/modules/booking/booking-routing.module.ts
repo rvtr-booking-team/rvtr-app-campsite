b1f9493f7d16eaa2c4ffbcba17215bdae409a75a
tew9 <tango.tew@outlook.com>
2020-04-27 21:03:46 -0500

    add routing to the booking link [#172282314]

--- angular/src/app/modules/booking/booking-routing.module.ts
+++ angular/src/app/modules/booking/booking-routing.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { BookingSearchComponent } from './booking/booking-search/booking-search.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: BookingSearchComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- angular/src/app/modules/booking/booking.module.ts
+++ angular/src/app/modules/booking/booking.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { BookingRoutingModule } from './booking-routing.module';
-
+import { BookingSearchComponent } from './booking/booking-search/booking-search.component';
+import { BookingFilterComponent } from './booking/booking-filter/booking-filter.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [BookingSearchComponent],
   imports: [
     CommonModule,
     BookingRoutingModule
-  ]
+  ],
+
 })
 export class BookingModule { }
